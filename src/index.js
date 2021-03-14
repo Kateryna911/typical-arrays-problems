8d31c9848242e4fe9a37fe6f9a2e1e1f3b93ccc8
Kateryna911 <ptica.katia@gmail.com>
2021-03-14 21:02:55 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 
 exports.min = function min (array) {
-  return Math.min.apply(null, this);
-}
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    return Math.min(...array);
+};
 
-exports.max = function max (array) {
-  return Math.max.apply(null, this);
-}
+exports.max = function max(array) {
+    if (array == undefined || array.length == 0) {
+        return 0;
+    }
+    return Math.max(...array);
+};
 
-exports.avg = function avg (array) {
+exports.avg = function avg(array) {
     if (array == undefined || array.length == 0) {
         return 0;
     }
